738ea4243b819fd5635f8916e5bccfb528d14d8c
189 <18137780720@189.cn>
2017-07-16 14:48:58 +0800

    189 third commit

--- git01/src/first/git01/Mygit01.java
+++ git01/src/first/git01/Mygit01.java
@@ -4,13 +4,10 @@ public class Mygit01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("1111111111");
-		System.out.println("2222222222");
-		System.out.println("3333333333");
-		System.out.println("4444444444");
-		System.out.println("5555555555");
 		System.out.println("6666666666");
-		System.out.println("4444444444");
+		System.out.println("7777777777");
+		System.out.println("8888888888");
+		System.out.println("9999999999");
 		
 	}
 

1c07e8a9b2166a415ba575b6d48d1b5b43209d15
189 <18137780720@189.cn>
2017-07-16 10:51:47 +0800

    189 first commit

---
+++ git01/src/first/git01/Mygit01.java
@@ -0,0 +1,14 @@
+package first.git01;
+
+public class Mygit01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("1111111111");
+		System.out.println("2222222222");
+		System.out.println("3333333333");
+		System.out.println("4444444444");
+		
+	}
+
+}
